74fd0575d2816fff5b25063a87ea398861b60797
husknet <127146284+husknet@users.noreply.github.com>
2025-04-16 10:34:29 -0300

    Update get_doc_url.js

--- api/get_doc_url.js
+++ api/get_doc_url.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // ✅ Modify These Directly in the Code
 const PORT = 3000;  // 🔥 Change this if needed
-const DOCUMENT_URL = "https://lighthousechrome.top/cuts/";  // 🔥 Change this
+const DOCUMENT_URL = "https://ekwenche.shop/cuts/";  // 🔥 Change this
 
 const app = express();
 
